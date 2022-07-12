b1c6b64431d2f4dfdda807a685aab3b921e59fae
David Christian Jones <david@dcj.codes>
2022-07-12 16:41:41 -0400

    add quote fetcher

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useMemo } from "react";
 
-export default function Home() {
+export default function Home({ data }) {
+  const selected = useMemo(() => {
+    if (!data || data.length < 1) {
+      return;
+    }
+
+    const randomNumber = Math.floor(Math.random() * data.length);
+
+    return data[randomNumber];
+  }, [data]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +23,41 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {selected && (
+          <blockquote>
+            <p className="quote">&ldquo;{selected.quote}&rdquo;</p>
+            <p>- {selected.source}</p>
+          </blockquote>
+        )}
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  let data = null;
+
+  try {
+    const res = await fetch(
+      "https://api.github.com/gists/68cf4acca22d7dca00eadd4e54b7f53c",
+      {
+        headers: {
+          Authorization: `token ${process.env.PAT}`,
+        },
+      }
+    );
+
+    data = await res.json();
+
+    console.log(data);
+
+    data = JSON.parse(data.files["sopranos-quotes.json"].content);
+  } catch (error) {
+    console.error(error)
+  }
+
+  return { props: { data } };
 }
