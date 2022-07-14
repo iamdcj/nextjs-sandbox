981a8c5dacec859bba877b10815809698bce0c7b
David Christian Jones <david@dcj.codes>
2022-07-14 13:09:49 -0400

    let's see

--- clients/firebase.js
+++ clients/firebase.js
@@ -1,6 +1,7 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 
  const returnFirebaseApp = () => {
+   console.log(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
   return initializeApp(
     process.env.NEXT_PUBLIC_FIREBASE_CONFIG
       ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
