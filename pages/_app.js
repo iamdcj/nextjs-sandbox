a5a36b88e190075e1656e226b4ad8a8c56e776d0
David Christian Jones <david@dcj.codes>
2022-07-18 16:02:48 -0400

    fix styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
+import DefaultLayout from "../layouts/default";
 import AuthenticationProvider from "../providers/authentication";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthenticationProvider>
-      <Component {...pageProps} />
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
     </AuthenticationProvider>
   );
 }
-
-
 
 export default MyApp;
